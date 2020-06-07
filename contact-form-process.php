fd2a931fbbd591de5fa67c705a7880ee1ef0c67e
Yooonn <yns@hawaii.edu>
2020-06-06 23:33:59 -1000

    PHP Mailer

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "viwe@naver.com";
+    $email_to = "essaninc@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
